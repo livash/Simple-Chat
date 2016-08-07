48a34e5f46c89f2aa95111b913b62a3a3e83f7ac
Olena Deering <olena.ivashyna@gmail.com>
2016-08-07 10:56:36 -0600

    add socket.io

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var socket = require('socket.io')(http);
 
 app.get('/chat', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+socket.on('connection', function(socket) {
+  console.log('a user connected');
+})
+
 http.listen(3000, function() {
-  console.log("Listening on *:3000");
+  console.log("Listening on port 3000");
 });
