bb66b7494b2e65550f097d18ae57cf6d867d04a3
Olena Deering <olena.ivashyna@gmail.com>
2016-08-08 22:14:36 -0600

    ui for numbers and operators

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,10 +1,56 @@
-let Main = React.createClass({
-  render: () => {
+class Numbers extends React.Component {
+  render() {
+    let numbers = [1,2,3,4,5,6,7,8,9,0];
     return (
-      <div>Hello from Olena</div>
+      <div className="numbers">
+        {numbers.map( number => (<button key={number}>{number}</button>))}
+      </div>
     );
   }
-});
+};
+
+class Operators extends React.Component {
+  render() {
+    let operators = [
+      {
+        name: "plus",
+        value: "+"
+      },
+      {
+        name: "minus",
+        value: "-"
+      },
+      {
+        name: "multiply",
+        value: "*"
+      },
+      {
+        name: "divide",
+        value: "/"
+      },
+    ]
+    return (
+      <div className="operators">
+        {operators.map( operator => {
+          return (
+            <button className="operator" key={operator.name}>{operator.value}</button>
+          );
+        })}
+      </div>
+    );
+  }
+};
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        <Numbers />
+        <Operators />
+      </div>
+    );
+  }
+};
 
 ReactDOM.render(
   <Main />,
