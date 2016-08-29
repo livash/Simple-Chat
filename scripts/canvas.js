c58581df8ff2fa1506c8bcdc3711f75260b93239
Olena Deering <olena.ivashyna@gmail.com>
2016-08-28 21:29:57 -0600

    canvas styles

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -2,4 +2,8 @@ var canvas = document.getElementById('canvas-example');
 var ctx = canvas.getContext('2d');
 
 ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 100, 100);
+ctx.fillRect(5, 5, 100, 100);
+ctx.fillStyle="red";
+ctx.fillRect(110,5, 100, 100);
+ctx.fillStyle = "yellow";
+ctx.fillRect(215, 5, 100, 100);
