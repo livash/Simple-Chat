4a36bb91c26b413b4b062447a4bdac8f38853bc4
Olena Deering <olena.ivashyna@gmail.com>
2016-08-28 21:55:00 -0600

    picture frame

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,13 +1,28 @@
-var canvas = document.getElementById('canvas-example');
-var ctx = canvas.getContext('2d');
+function drawOverlapRect(id) {
+  var canvas = document.getElementById(id);
+  var ctx = canvas.getContext('2d');
 
-ctx.fillStyle = "rgb(200,0,0)";
-ctx.fillRect(5, 5, 100, 100);
-ctx.fillStyle="rgba(0,0,200,0.5)";
-ctx.fillRect(55, 55, 100, 100);
+  ctx.fillStyle = "rgb(200,0,0)";
+  ctx.fillRect(5, 5, 100, 100);
+  ctx.fillStyle="rgba(0,0,200,0.5)";
+  ctx.fillRect(55, 55, 100, 100);
 
-// outline of a rectangle
-ctx.strokeRect(250,150, 25,25);
+  // outline of a rectangle
+  ctx.strokeRect(250,150, 25,25);
 
-//clear rectangle, clear space
-ctx.clearRect(25,25,25,25);
+  //clear rectangle, clear space
+  ctx.clearRect(25,25,25,25);
+}
+
+// picture frame
+function drawPictureFrame(id) {
+  var canvas = document.getElementById(id);
+  var ctx = canvas.getContext('2d');
+
+  ctx.fillRect(5,5,180,180);
+  ctx.clearRect(25,25,140,140);
+  ctx.strokeRect(40,40,110,110);
+}
+
+drawOverlapRect('canvas-example');
+drawPictureFrame('picture-frame');
