c24af0dd7ada0306c5adc784f6fa96d6ab58987b
Olena Deering <olena.ivashyna@gmail.com>
2016-08-28 21:40:50 -0600

    use fillRect, strokeRect, clearRect

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -5,3 +5,9 @@ ctx.fillStyle = "rgb(200,0,0)";
 ctx.fillRect(5, 5, 100, 100);
 ctx.fillStyle="rgba(0,0,200,0.5)";
 ctx.fillRect(55, 55, 100, 100);
+
+// outline of a rectangle
+ctx.strokeRect(250,150, 25,25);
+
+//clear rectangle, clear space
+ctx.clearRect(25,25,25,25);
