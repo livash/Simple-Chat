4a5daa373e2b117124a1911bbfae9b01fb27f3d3
Olena Deering <olena.ivashyna@gmail.com>
2016-08-28 21:35:04 -0600

    draw overlaying rectangles

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,9 +1,7 @@
 var canvas = document.getElementById('canvas-example');
 var ctx = canvas.getContext('2d');
 
-ctx.fillStyle = "green";
+ctx.fillStyle = "rgb(200,0,0)";
 ctx.fillRect(5, 5, 100, 100);
-ctx.fillStyle="red";
-ctx.fillRect(110,5, 100, 100);
-ctx.fillStyle = "yellow";
-ctx.fillRect(215, 5, 100, 100);
+ctx.fillStyle="rgba(0,0,200,0.5)";
+ctx.fillRect(55, 55, 100, 100);
