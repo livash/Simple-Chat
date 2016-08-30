4e27f588ae68161701f06dda83ace7758712b6dc
Olena Deering <olena.ivashyna@gmail.com>
2016-08-29 21:15:11 -0600

    smiley face

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -52,6 +52,23 @@ function drawTriangle(id) {
   ctx.fill();
 }
 
+function drawSmiley(id) {
+  var ctx = initContext(id);
+  ctx.lineWidth = 3;
+  ctx.beginPath();
+  ctx.arc(75,75,50,0,Math.PI*2,true); // Outer circle
+  ctx.fillStyle = 'yellow';
+  ctx.fill();
+  ctx.moveTo(110,75);
+  ctx.arc(75,75,35,0,Math.PI,false);  // Mouth (clockwise)
+  ctx.moveTo(65,65);
+  ctx.arc(60,65,5,0,Math.PI*2,true);  // Left eye
+  ctx.moveTo(95,65);
+  ctx.arc(90,65,5,0,Math.PI*2,true);  // Right eye
+  ctx.stroke();
+}
+
 drawOverlapRect('canvas-example');
 drawPictureFrame('picture-frame');
 drawTriangle('triangle-example');
+drawSmiley('smiley-face');
